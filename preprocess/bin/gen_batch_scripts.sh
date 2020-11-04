83e5d77af1994d89cf20c6833038869b55c04d2e
eculler <eculler@gmail.com>
2020-11-04 16:14:00 -0700

    batch scripts on summit

--- preprocess/bin/gen_batch_scripts.sh
+++ preprocess/bin/gen_batch_scripts.sh
@@ -7,54 +7,56 @@ LAT_NAME=lat
 TIME_NAME=time
 
 # Configure paths
-WORKDIR=/projects/elcu4811/landslide.hydromet/landslide.hydromet.git
+WORKDIR=/projects/elcu4811/landslide.hydromet/landslide.hydromet.git/preprocess
 DATADIR=/scratch/summit/elcu4811/landslide.hydromet.data
 OUTDIR=/projects/elcu4811/landslide.hydromet/landslide.hydromet.out/$PRECIPNAME
-SRCDIR=${WORKDIR}/preprocess/src
-BATCHDIR=${OUTDIR}/batch
-if [TEMPLATE==test]
-  TEMPLATE="batch_template_test.txt"
+SRCDIR=${WORKDIR}/src
+BATCHDIR=${SRCDIR}/batch
+if [ $TEMPLATE == "test" ]
+then
+  TEMPLATE_PATH="batch_template_test.txt"
+  MONTH=10
+  STARTYEAR=2018
+  ENDYEAR=2018
 else
-  TEMPLATE="batch_template.txt"
+  TEMPLATE_PATH="batch_template.txt"
+  MONTH=""
+  STARTYEAR=2014
+  ENDYEAR=2020
 fi
-mkdir -p $WORKDIR $DATADIR $OUTDIR $SRCDIR $BATCHDIR
+mkdir -p $WORKDIR $OUTDIR $BATCHDIR
 
 # Make batch script for each year
-for YEAR in {2014..2020}; do
+for ((YEAR=STARTYEAR; YEAR<=ENDYEAR; YEAR++)); do
 
-BATCHPATH=${BATCHDIR}/${PRECIPNAME}.${YEAR}.batch
+BATCHPATH=${BATCHDIR}/${PRECIPNAME}.${TEMPLATE}.${YEAR}.batch
 case ${PRECIPNAME} in
   nldas)
-    PRECIP="precipitation/NLDAS2/NLDAS_FORA0125_H.A${YEAR}\*.\*.002.grb.SUB.nc4"
+    PRECIP="precipitation/NLDAS2/NLDAS_FORA0125_H.A${YEAR}${MONTH}\*.\*.002.grb.SUB.nc4"
     PRECIP_NAME=APCP
     ;;
   imergf)
-    PRECIP="precipitation/IMERG.final/3B-HHR.MS.MRG.3IMERG.${YEAR}\*-S\*.nc4"
+    PRECIP="precipitation/IMERGF/${YEAR}/3B-HHR.MS.MRG.3IMERG.${YEAR}${MONTH}\*-S\*.nc4"
     PRECIP_NAME=precipitationCal
     ;;
   imerge)
-    PRECIP="precipitation/IMERG.early/3B-HHR-E.MS.MRG.3IMERG.${YEAR}\*-S\*.nc4"
+    PRECIP="precipitation/IMERGE/${YEAR}/3B-HHR-E.MS.MRG.3IMERG.${YEAR}${MONTH}\*-S\*.nc4"
     PRECIP_NAME=precipitationCal
     ;;
   mrms)
-    PRECIP="precipitation/MRMS/"
+    PRECIP="precipitation/MRMS/GaugeCorr_QPE_01H_00.00_${YEAR}${MONTH}\*-\*.nc"
     PRECIP_NAME=precip
     ;;
 esac
 
 # Initialize file
-cp ${WORKDIR}/preprocess/bin/${TEMPLATE} ${BATCHPATH}
+echo $BATCHPATH
+cp ${SRCDIR}/${TEMPLATE_PATH} ${BATCHPATH}
 
 # Write command to start container, run python script
 cat >> ${BATCHPATH} << EOL
-singularity exec ${WORKDIR}/hydrological.processes.202011.img \\
-  conda run -n hydrometenv \\
-  python ${SRCDIR}/landslide_precip.py \\
-  ${DATADIR}/${PRECIP} \\
-  ${DATADIR}/landslide/landslides.csv \\
-  ${OUTDIR}/${PRECIPNAME}.${YEAR}.csv \\
-  ${LON_NAME} ${LAT_NAME} ${PRECIP_NAME} ${TIME_NAME} \\
-  ${LOGLEVEL}
+singularity run --bind /scratch/summit ${WORKDIR}/hydrological.processes.202011.sif bash -c \\
+  'conda activate hydrometenv && python ${SRCDIR}/landslide_precip.py ${DATADIR}/${PRECIP} ${DATADIR}/landslide/landslides.csv ${OUTDIR}/${PRECIPNAME}.${YEAR}.csv ${LON_NAME} ${LAT_NAME} ${PRECIP_NAME} ${TIME_NAME} ${LOGLEVEL}'
 EOL
 
 done
